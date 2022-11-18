fdd7d56132ac2e182470faac7ac80649a26f4b74
Pavel Šmíd <78.78@seznam.cz>
2022-11-18 07:39:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup as bs
 import requests
 from datetime import datetime
 
-# okno 
-window =Tk()
+# okno
+window = Tk()
 window.title("Převod měn")
-window.minsize(300, 200) # velikost okna
-window.resizable(False, False) # 
+window.minsize(300, 200)  # velikost okna
+window.resizable(False, False)  #
 window.config(bg="#222")
 window.iconbitmap("icon_many.ico")
 
@@ -20,7 +20,7 @@ def kurz_euro():
 
     euro = soup.find(id="rate_eur")
     euro = euro.getText()
-    euro_float = float(euro.replace(",","."))
+    euro_float = float(euro.replace(",", "."))
     return euro_float
 
 
@@ -28,14 +28,13 @@ def kurz_euro():
 def count_currency_czk():
     amount_czk = float(amount_input.get()) * kurz_euro()
     result_label_czk["text"] = round(amount_czk, 2)
- 
+
 
 # funkce 1 euro = CNB
-def count_currency_euro():   
-    
+def count_currency_euro():
+
     amount_eur = float(amount_input.get()) / kurz_euro()
     result_label_euro["text"] = round(amount_eur, 2)
-
 
 
 def ulozit_soubor():
@@ -43,22 +42,21 @@ def ulozit_soubor():
     zapis_czk = str(result_label_czk["text"])
     datum = datumcas()
     castka = amount_input.get()
-    
+
     with open("Převod měny.txt", "a", encoding="utf-8") as f:
-       f.write("\n")
-       f.write(datum)
-       f.write("\n")
-       f.write("Částka: ")
-       f.write(castka)
-       f.write("\n")
-       f.write(zapis_euro) 
-       f.write(" euro")
-       f.write("\n")
-       f.write(zapis_czk)
-       f.write(" czk")
-       f.write("\n")
-       f.write("-" * 18 )
-    
+        f.write("\n")
+        f.write(datum)
+        f.write("\n")
+        f.write("Částka: ")
+        f.write(castka)
+        f.write("\n")
+        f.write(zapis_euro)
+        f.write(" euro")
+        f.write("\n")
+        f.write(zapis_czk)
+        f.write(" czk")
+        f.write("\n")
+        f.write("-" * 18)
 
 
 def datumcas():
@@ -85,25 +83,27 @@ result_label_czk = Label(text="0", font=("Helvetica", 15), bg="#222", fg="white"
 result_label_czk.grid(row=2, column=0)
 
 # kurz CNB
-euro_CNB = Label(text=("euro","=",kurz_euro(),"ckz","-","kurz","ČNB"), font=("Helvetica", 11), bg="#222", fg="white")
+euro_CNB = Label(
+    text=("euro", "=", kurz_euro(), "ckz", "-", "kurz", "ČNB"),
+    font=("Helvetica", 11),
+    bg="#222",
+    fg="white",
+)
 euro_CNB.grid(row=3, column=0)
 
 # čas + datum
-cas_datum = Label(text = (datumcas()  ), font=("Helvetica", 11), bg="#222", fg="white")
+cas_datum = Label(text=(datumcas()), font=("Helvetica", 11), bg="#222", fg="white")
 cas_datum.grid(row=4, column=0)
 
 
 # Tlačítka
-count_button = Button(text=" EUR ", font=("Helvetica", 15),
-command=count_currency_euro)
+count_button = Button(text=" EUR ", font=("Helvetica", 15), command=count_currency_euro)
 count_button.grid(row=1, column=1, padx=10, pady=10)
 
-count_button = Button(text=" CZK ", font=("Helvetica", 15),
-command=count_currency_czk)
+count_button = Button(text=" CZK ", font=("Helvetica", 15), command=count_currency_czk)
 count_button.grid(row=2, column=1, padx=10, pady=10)
 
-count_button = Button(text="Uložit", font=("Helvetica", 15),
-command=ulozit_soubor)
+count_button = Button(text="Uložit", font=("Helvetica", 15), command=ulozit_soubor)
 count_button.grid(row=3, column=1, padx=10, pady=10)
 
 
